c7bd4a2307141d180c79c6cf01018a0505ae98fb
Pixoll <pixoll.gamer@gmail.com>
2024-12-14 22:26:52 -0300

    Merge with english type definition

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,12 +1,13 @@
 import 'i18next';
 import spanish from "../translations/es.json";
+import english from "../translations/en.json";
 
 declare module "i18next" {
   // noinspection JSUnusedGlobalSymbols
   interface CustomTypeOptions {
     defaultNs: "global";
     resources: {
-      global: typeof spanish;
+      global: (typeof spanish) & (typeof english);
     };
   }
 }
