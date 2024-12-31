e22ec9d5a632b19cf38e3075b6836de8c7853829
benjag22 <benjagonzalez2022@udec.cl>
2024-12-31 17:11:28 -0300

    Add selector component on referencesValidate
    Added a selector to filter and display data by status to the references

--- src/core/components/adminPage/ReferenceValidated.tsx
+++ src/core/components/adminPage/ReferenceValidated.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Alert, Button, Card, Container, Table } from "react-bootstrap";
 import { City, Journal, Author, Reference } from "../../hooks";
+import { BadgeX, CheckCircle, PlusCircle, RefreshCw } from "lucide-react";
 import Pagination from "../search/Pagination";
 import CSVReferenceDisplay from "./CSVReferenceDisplay";
 import { CSVReference } from "./FoodsFromCsv";
@@ -12,7 +13,29 @@ type ReferenceValidatedProps = {
   authorsInfo: Author[];
   journalsInfo: Journal[];
   referencesInfo: Reference[];
-  handleView: (change:boolean) => void;
+  handleView: (change: boolean) => void;
+};
+enum Flag {
+  INVALID = 0,
+  VALID = 1,
+  IS_NEW = 1 << 1,
+  UPDATED = 1 << 2,
+}
+
+const getIconForFlags = (flags: number) => {
+  if (flags === Flag.INVALID) {
+    return <BadgeX color="red"></BadgeX>;
+  }
+  if (flags & Flag.IS_NEW) {
+    return <PlusCircle color="blue" />;
+  }
+  if (flags & Flag.UPDATED) {
+    return <RefreshCw color="orange" />;
+  }
+  if (flags & Flag.VALID) {
+    return <CheckCircle color="green" />;
+  }
+  return null;
 };
 
 export default function ReferenceValidated({
@@ -21,8 +44,12 @@ export default function ReferenceValidated({
   authorsInfo,
   journalsInfo,
   referencesInfo,
-  handleView
+  handleView,
 }: ReferenceValidatedProps) {
+
+  const [view, setView] = useState("list");
+  const [selectedReference, setSelectedReference] = useState<CSVReference | null>(null);
+
   const [currentPage, setCurrentPage] = useState(1);
   const recordsPerPage = 7;
   const [filteredData, setFilteredData] = useState(data);
@@ -31,26 +58,72 @@ export default function ReferenceValidated({
   const lastIndex = currentPage * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
   const records = filteredData.slice(firstIndex, lastIndex);
-
-  useEffect(() => {
-    setFilteredData(data);
-  }, [data]);
-
   const changePage = (page: number) => {
     if (page >= 1 && page <= npage) {
       setCurrentPage(page);
     }
   };
-  const handleReferences = () =>{
-    console.log("Se ha enviado las referencias") 
-    handleView(false)
-  }
-  const [view, setView] = useState("list");
-  const [selectedReference, setSelectedReference] =
-    useState<CSVReference | null>(null);
+
+  useEffect(() => {
+    setFilteredData(data);
+  }, [data]);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("Mostrar todos");
+  const dropdownRef = useRef(null);
+  const options = ["Mostrar todos", "Invalidos", "Actualizados", "Validos"];
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && 
+          !(dropdownRef.current as HTMLElement).contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+  
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+  const handleReferences = () => {
+    console.log("Se ha enviado las referencias");
+    handleView(false);
+  };
+
+  const handleSelect = (option: string) => {
+      setSelectedOption(option);
+      setIsOpen(false);
+  };
 
   return (
     <Container fluid className="py-4">
+      <h4>Mostrar </h4>
+      <div className="dropdown-wrapper" ref={dropdownRef}>
+        <div className="dropdown-button" onClick={() => setIsOpen(!isOpen)}>
+          <span id="selected-option">{selectedOption}</span>
+          <span>
+            <i
+              className={`fa-solid fa-caret-down ${
+                isOpen ? "fa-rotate-180" : ""
+              }`}
+            ></i>
+          </span>
+        </div>
+        <div className={`dropdown-menu ${isOpen ? "active" : ""}`}>
+          {options.map((option, index) => (
+            <div
+              key={index}
+              className={`order-item ${
+                selectedOption === option ? "selected" : ""
+              }`}
+              onClick={() => handleSelect(option)}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      </div>
       {view === "list" && (
         <Card>
           <Card.Header as="h2" className="titleStyle">
@@ -74,7 +147,7 @@ export default function ReferenceValidated({
                   <tbody>
                     {records.map((item, index) => (
                       <tr key={index}>
-                        <td>{firstIndex + index + 1}</td>
+                        <td>{getIconForFlags(item.flags)}</td>
                         <td>
                           {item.title?.parsed || item.title?.raw || "N/A"}
                         </td>
@@ -120,12 +193,12 @@ export default function ReferenceValidated({
           <Card.Body>
             {selectedReference && (
               <div className="mb-4">
-                <CSVReferenceDisplay 
-                reference={selectedReference}
-                authorsInfo={authorsInfo}
-                citiesInfo={citiesInfo}
-                journalsInfo={journalsInfo}
-                referencesInfo={referencesInfo} 
+                <CSVReferenceDisplay
+                  reference={selectedReference}
+                  authorsInfo={authorsInfo}
+                  citiesInfo={citiesInfo}
+                  journalsInfo={journalsInfo}
+                  referencesInfo={referencesInfo}
                 />
               </div>
             )}
@@ -142,10 +215,10 @@ export default function ReferenceValidated({
           </Card.Body>
         </Card>
       )}
-    <Container>
-      <button className="button-form-of-food" onClick={handleReferences}>
-        Enviar
-      </button>
+      <Container>
+        <button className="button-form-of-food" onClick={handleReferences}>
+          Enviar
+        </button>
       </Container>
     </Container>
   );
