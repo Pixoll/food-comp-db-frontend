bf519f3481bce34da423538a8d08cb5f44da2c18
benjag22 <benjagonzalez2022@udec.cl>
2025-05-09 12:45:02 -0400

    Add diferent styles to compare button of food

--- src/core/components/search/FoodResultsTable.tsx
+++ src/core/components/search/FoodResultsTable.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp, Plus, Minus } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
@@ -35,7 +35,8 @@ export default function FoodResultsTable({
   const navigate = useNavigate();
   const { state } = useAuth();
   const { token } = state;
-  const { comparisonFoods, addToComparison, removeFromComparison } = useComparison();
+  const { comparisonFoods, addToComparison, removeFromComparison } =
+    useComparison();
   const { t, i18n } = useTranslation();
   const [selectedSort, setSelectedSort] = useState(SortType.NAME);
   const [sortOrder, setSortOrder] = useState(SortOrder.ASC);
@@ -72,28 +73,31 @@ export default function FoodResultsTable({
   };
   const exportData = async (codes: string[]) => {
     try {
-      const response = await axios.get(`http://localhost:3000/api/xlsx?codes=${codes.join(",")}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        responseType: 'blob',
-      });
-      
-      const blob = new Blob([response.data], { 
-        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' 
+      const response = await axios.get(
+        `http://localhost:3000/api/xlsx?codes=${codes.join(",")}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          responseType: "blob",
+        }
+      );
+
+      const blob = new Blob([response.data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
       });
       const url = window.URL.createObjectURL(blob);
-      
-      let fileName = 'foods_data.xlsx';
-      const contentDisposition = response.headers['content-disposition'];
+
+      let fileName = "foods_data.xlsx";
+      const contentDisposition = response.headers["content-disposition"];
       if (contentDisposition) {
         const match = contentDisposition.match(/filename="(.+)"/);
         if (match && match[1]) {
           fileName = match[1];
         }
       }
-      
-      const link = document.createElement('a');
+
+      const link = document.createElement("a");
       link.href = url;
       link.download = fileName;
       document.body.appendChild(link);
@@ -102,7 +106,6 @@ export default function FoodResultsTable({
       window.URL.revokeObjectURL(url);
     } catch (error) {
       console.error(error);
-
     }
   };
   useEffect(() => {
@@ -126,8 +129,14 @@ export default function FoodResultsTable({
           const subspeciesA = a.subspecies?.toLowerCase() ?? "";
           const scientificNameB = b.scientificName?.toLowerCase() ?? "";
           const subspeciesB = b.subspecies?.toLowerCase() ?? "";
-          stringA = (subspeciesA ? `${scientificNameA} ${subspeciesA}` : scientificNameA) || "~";
-          stringB = (subspeciesB ? `${scientificNameB} ${subspeciesB}` : scientificNameB) || "~";
+          stringA =
+            (subspeciesA
+              ? `${scientificNameA} ${subspeciesA}`
+              : scientificNameA) || "~";
+          stringB =
+            (subspeciesB
+              ? `${scientificNameB} ${subspeciesB}`
+              : scientificNameB) || "~";
           break;
         }
       }
@@ -150,11 +159,20 @@ export default function FoodResultsTable({
       setCurrentPage(maxPage);
     }
     // eslint-disable-next-line
-  }, [data, selectedLanguage, selectedSort, sortOrder, resultsPerPage, searchForName]);
+  }, [
+    data,
+    selectedLanguage,
+    selectedSort,
+    sortOrder,
+    resultsPerPage,
+    searchForName,
+  ]);
 
   const handleSortClick = (type: SortType) => {
     if (selectedSort === type) {
-      setSortOrder(sortOrder === SortOrder.ASC ? SortOrder.DESC : SortOrder.ASC);
+      setSortOrder(
+        sortOrder === SortOrder.ASC ? SortOrder.DESC : SortOrder.ASC
+      );
     } else {
       setSelectedSort(type);
       setSortOrder(SortOrder.ASC);
@@ -183,9 +201,15 @@ export default function FoodResultsTable({
               onChange={(e) => setSelectedSort(+e.target.value)}
               className="form-select"
             >
-              <option value={SortType.CODE}>{t("Table_FoodResults.code")}</option>
-              <option value={SortType.NAME}>{t("Table_FoodResults.name")}</option>
-              <option value={SortType.SCIENTIFIC_NAME}>{t("Table_FoodResults.scientific_name")}</option>
+              <option value={SortType.CODE}>
+                {t("Table_FoodResults.code")}
+              </option>
+              <option value={SortType.NAME}>
+                {t("Table_FoodResults.name")}
+              </option>
+              <option value={SortType.SCIENTIFIC_NAME}>
+                {t("Table_FoodResults.scientific_name")}
+              </option>
             </select>
           </Col>
 
@@ -197,15 +221,18 @@ export default function FoodResultsTable({
               className="form-select"
             >
               <option value={SortOrder.ASC}>{t("Table.sort.ascending")}</option>
-              <option value={SortOrder.DESC}>{t("Table.sort.descending")}</option>
+              <option value={SortOrder.DESC}>
+                {t("Table.sort.descending")}
+              </option>
             </select>
           </Col>
           <Col xs={12} sm={4}>
-          <button 
-            className="export-button"
-            onClick={()=>exportData(data.map(f=>f.code))}>
-            Exportar resultados
-          </button>
+            <button
+              className="export-button"
+              onClick={() => exportData(data.map((f) => f.code))}
+            >
+              Exportar resultados
+            </button>
           </Col>
           <Col xs={12} sm={4}>
             <h5>{t("Table.results_per_page")}</h5>
@@ -229,86 +256,104 @@ export default function FoodResultsTable({
         <>
           <table className="content-table-foods">
             <thead>
-            <tr>
-              <th>
-                <div onClick={() => handleSortClick(SortType.CODE)}>
-                  {selectedSort === SortType.CODE && (
-                    sortOrder === SortOrder.ASC ? <ArrowUp height={24}/> : <ArrowDown height={24}/>
-                  )}
-                  {t("Table_FoodResults.code")}
-                </div>
-              </th>
-              <th>
-                <div onClick={() => handleSortClick(SortType.NAME)}>
-                  {selectedSort === SortType.NAME && (
-                    sortOrder === SortOrder.ASC ? <ArrowUp height={24}/> : <ArrowDown height={24}/>
-                  )}
-                  {t("Table_FoodResults.name")}
-                </div>
-              </th>
-              <th>
-                <div onClick={() => handleSortClick(SortType.SCIENTIFIC_NAME)}>
-                  {selectedSort === SortType.SCIENTIFIC_NAME && (
-                    sortOrder === SortOrder.ASC ? <ArrowUp height={24}/> : <ArrowDown height={24}/>
-                  )}
-                  {t("Table_FoodResults.scientific_name")}
-                </div>
-              </th>
-              <th>
-                {t("Table_FoodResults.action")}
-              </th>
-              <th>
-                Comparar
-              </th>
-            </tr>
+              <tr>
+                <th>
+                  <div onClick={() => handleSortClick(SortType.CODE)}>
+                    {selectedSort === SortType.CODE &&
+                      (sortOrder === SortOrder.ASC ? (
+                        <ArrowUp height={24} />
+                      ) : (
+                        <ArrowDown height={24} />
+                      ))}
+                    {t("Table_FoodResults.code")}
+                  </div>
+                </th>
+                <th>
+                  <div onClick={() => handleSortClick(SortType.NAME)}>
+                    {selectedSort === SortType.NAME &&
+                      (sortOrder === SortOrder.ASC ? (
+                        <ArrowUp height={24} />
+                      ) : (
+                        <ArrowDown height={24} />
+                      ))}
+                    {t("Table_FoodResults.name")}
+                  </div>
+                </th>
+                <th>
+                  <div
+                    onClick={() => handleSortClick(SortType.SCIENTIFIC_NAME)}
+                  >
+                    {selectedSort === SortType.SCIENTIFIC_NAME &&
+                      (sortOrder === SortOrder.ASC ? (
+                        <ArrowUp height={24} />
+                      ) : (
+                        <ArrowDown height={24} />
+                      ))}
+                    {t("Table_FoodResults.scientific_name")}
+                  </div>
+                </th>
+                <th>{t("Table_FoodResults.action")}</th>
+                <th>Comparar</th>
+              </tr>
             </thead>
             <tbody>
-            {records.map((item) => (
-              <tr key={item.code}>
-                <td data-label="Code">{item.code}</td>
-                <td data-label="Nombre">{item.commonName[selectedLanguage] || "N/A"}</td>
-                <td data-label="Nombre científico">
-                  {(item.subspecies
-                    ? `${item.scientificName} ${item.subspecies.toLowerCase()}`
-                    : item.scientificName) || "N/A"}
-                </td>
-                <td>
-                  <button
-                    onClick={() => toFoodDetail(item.code)}
-                    style={{ marginRight: state.isAuthenticated ? "10px" : "" }}
-                  >
-                    {t("Table_FoodResults.details")}
-                  </button>
-                  {state.isAuthenticated && (
+              {records.map((item) => (
+                <tr key={item.code}>
+                  <td data-label="Code">{item.code}</td>
+                  <td data-label="Nombre">
+                    {item.commonName[selectedLanguage] || "N/A"}
+                  </td>
+                  <td data-label="Nombre científico">
+                    {(item.subspecies
+                      ? `${
+                          item.scientificName
+                        } ${item.subspecies.toLowerCase()}`
+                      : item.scientificName) || "N/A"}
+                  </td>
+                  <td>
                     <button
-                      onClick={() => toModifyFoodDetail(item.code)}
-                      style={{ backgroundColor: "#3b7791" }}
+                      onClick={() => toFoodDetail(item.code)}
+                      style={{
+                        marginRight: state.isAuthenticated ? "10px" : "",
+                      }}
                     >
-                      {t("Table_FoodResults.modify")}
+                      {t("Table_FoodResults.details")}
                     </button>
-                  )}
-                </td>
-                <th>
-                  <button>
-                    {comparisonFoods.map(f=>f.code).includes(item.code) ? (
+                    {state.isAuthenticated && (
+                      <button
+                        onClick={() => toModifyFoodDetail(item.code)}
+                        style={{ backgroundColor: "#3b7791" }}
+                      >
+                        {t("Table_FoodResults.modify")}
+                      </button>
+                    )}
+                  </td>
+                  <th>
+                    {comparisonFoods.map((f) => f.code).includes(item.code) ? (
                       <button
                         onClick={() => removeFromComparison(item.code)}
-                        className="btn btn-danger"
+                        className="comparison-button remove"
+                        aria-label="Eliminar"
                       >
-                        Eliminar 
+                        <Minus size={16} />
                       </button>
                     ) : (
                       <button
-                        onClick={() => addToComparison({code: item.code,name: item.commonName[selectedLanguage] ?? ""})}
-                        className="btn btn-primary"
+                        onClick={() =>
+                          addToComparison({
+                            code: item.code,
+                            name: item.commonName[selectedLanguage] ?? "",
+                          })
+                        }
+                        className="comparison-button add"
+                        aria-label="Comparar"
                       >
-                        Comparar
+                        <Plus size={16} />
                       </button>
                     )}
-                  </button>
-                </th>
-              </tr>
-            ))}
+                  </th>
+                </tr>
+              ))}
             </tbody>
           </table>
           {npage > 1 && (
