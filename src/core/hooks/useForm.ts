ac61cb0efbe2573ccb71733f4c91adde0d8d1015
benjag22 <benjagonzalez2022@udec.cl>
2025-05-23 19:18:24 -0400

    feat: add function for different levels of nesting in useForm

--- src/core/hooks/useForm.ts
+++ src/core/hooks/useForm.ts
@@ -4,16 +4,62 @@ import { ChangeEvent, useState } from "react";
 type FormState<T> = {
   [K in keyof T]: T[K];
 };
+function setNestedValue(obj: any, path: string, value: any): any {
+  if (!path) return value;
 
+  const result = Array.isArray(obj) ? [...obj] : { ...obj };
+
+  const pathParts = path.split('.');
+  let current = result;
+
+  for (let i = 0; i < pathParts.length - 1; i++) {
+    const part = pathParts[i];
+
+    const arrayMatch = part.match(/^(.+)\[(\d+)\]$/);
+
+    if (arrayMatch) {
+      const [, arrayName, index] = arrayMatch;
+
+      if (!current[arrayName]) {
+        current[arrayName] = [];
+      }
+
+      if (!current[arrayName][+(index)]) {
+        current[arrayName][+(index)] = {};
+      }
+
+      current = current[arrayName][+(index)];
+    } else {
+      if (!current[part]) {
+        current[part] = {};
+      }
+
+      current = current[part];
+    }
+  }
+
+  const lastPart = pathParts[pathParts.length - 1];
+  const arrayMatch = lastPart.match(/^(.+)\[(\d+)\]$/);
+
+  if (arrayMatch) {
+    const [, arrayName, index] = arrayMatch;
+    if (!current[arrayName]) {
+      current[arrayName] = [];
+    }
+    current[arrayName][+(index)] = value;
+  } else {
+    current[lastPart] = value;
+  }
+
+  return result;
+}
 export function useForm<T extends object>(initialForm: FormState<T>) {
   const [formState, setFormState] = useState<FormState<T>>(initialForm);
 
   const onInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    console.log(target);
     const { name, value } = target;
-    setFormState({
-      ...formState,
-      [name]: value
-    });
+    setFormState((prevState: any) => setNestedValue(prevState, name, value));
   };
 
   const onResetForm = () => {
